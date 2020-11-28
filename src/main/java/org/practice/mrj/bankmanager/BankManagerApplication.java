30aa5d8da9e81508abd8ba83e3f2c9deb5547689
NineDawn <a1204969730@gmail.com>
2020-11-28 14:29:13 +0800

    init

---
+++ src/main/java/org/practice/mrj/bankmanager/BankManagerApplication.java
@@ -0,0 +1,13 @@
+package org.practice.mrj.bankmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/practice/mrj/bankmanager/BankManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.practice.mrj.bankmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
